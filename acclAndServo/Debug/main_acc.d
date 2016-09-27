35fff4e3d435bff69975e83a1f21fb9f3dcca099
Amir <amir_rajabifar@yahoo.com.au>
2016-09-27 13:29:02 +0800

    Add acclAndServo example project

---
+++ acclAndServo/Debug/main_acc.d
@@ -0,0 +1,15 @@
+# FIXED
+
+main_acc.obj: ../main_acc.c
+main_acc.obj: /home/amir/ti/ccsv6/ccs_base/msp430/include/msp430fr5739.h
+main_acc.obj: /home/amir/ti/ccsv6/ccs_base/msp430/include/in430.h
+main_acc.obj: /home/amir/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include/intrinsics.h
+main_acc.obj: /home/amir/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include/intrinsics_legacy_undefs.h
+main_acc.obj: ../FR_EXP.h
+
+../main_acc.c: 
+/home/amir/ti/ccsv6/ccs_base/msp430/include/msp430fr5739.h: 
+/home/amir/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+/home/amir/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include/intrinsics.h: 
+/home/amir/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include/intrinsics_legacy_undefs.h: 
+../FR_EXP.h: 
---
+++ acclAndServo/Debug/subdir_vars.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430fr5739.cmd 
+
+C_SRCS += \
+../main_acc.c 
+
+OBJS += \
+./main_acc.obj 
+
+C_DEPS += \
+./main_acc.d 
+
+C_DEPS__QUOTED += \
+"main_acc.d" 
+
+OBJS__QUOTED += \
+"main_acc.obj" 
+
+C_SRCS__QUOTED += \
+"../main_acc.c" 
+
+
